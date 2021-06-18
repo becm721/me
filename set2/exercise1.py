efdd4f29eaef447eee2c7a653da9c93a63c63672
Rebecca Martinez <becm721@gmail.com>
2021-06-18 13:47:14 +1000

    tried to push week 2

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -13,17 +13,22 @@ print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+# it will print the set letters/symbols that equal "some_words" in a list format
 for word in some_words:
-    print(word)
+    print(word)  # it printed 'what, does, this, line, do, ?' in a list formart
 
+#it will print the set words within the square brackets which involve defining lists
 for x in some_words:
-    print(x)
+    print(x) #it printed 'what, does, this, line, do, ?' in a list formart
 
-print(some_words)
+# prints what equals "some_words", the equal value means to the user and computer what components are associted when typing that phrase
+print(some_words) #printed '['what', 'does', 'this', 'line', 'do', '?']'
 
+#this is used as a boolean and determines whether or not in the set list within "some_words" it has more than three words
 if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+    print('some_words contains more than 3 words') #has more than three words so it printed 'some_words contains ore than 3 words'
 
+# platform.uname() provides information on the computer such as the OS
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
@@ -32,4 +37,4 @@ def usefulFunction():
     """
     print(platform.uname())
 
-usefulFunction()
+usefulFunction() # it printed uname_result(system='Windows', node='DESKTOP-7LDOP9I', release='10', version='10.0.19041', machine='AMD64', processor='Intel64 Family 6 Model 142 Stepping 10, GenuineIntel')
--- set2/exercise3.py
+++ set2/exercise3.py
@@ -7,7 +7,7 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    return None
+    return a_number % 2 != 0
 
 
 def fix_it(moves=True, should_move=True):
@@ -25,7 +25,12 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+    if moves == should_move:
+        return "No Problem"
+    elif moves and not should_move:
+        return "Duct Tape"
+    elif not moves and should_move:
+        return "WD-40"
 
 
 def loops_1a():
@@ -35,7 +40,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    star_list = []
+    for i in range(10):
+        star_list.append("*")
+    return star_list
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -45,7 +53,10 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    my_list = []
+    for i in range(number_of_items):
+        my_list.append(symbol)
+    return my_list
 
 
 def loops_2():
@@ -66,7 +77,11 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    star_square = []
+    for j in range(10):
+        star_square.append(loops_1c(number_of_items=10, symbol="*"))
+
+    return star_square
 
 
 def loops_3():
@@ -90,7 +105,14 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    number_square = []
+    for i in range(10):
+        number_row = []
+        for j in range(10):
+            number_row.append(str(i))
+        number_square.append(number_row)
+
+    return number_square
 
 
 def loops_4():
@@ -110,7 +132,14 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+    number_square = []
+    for i in range(10):
+        number_row = []
+        for j in range(10):
+            number_row.append(str(j))
+        number_square.append(number_row)
+
+    return number_square
 
 
 def loops_5():
@@ -137,7 +166,14 @@ def loops_5():
         "There are {} green bottles".format(8)
     you'll come to see the pros and cons of each over time.
     """
-    return None
+    number_square = []
+    for i in range(10):
+        cordinates_row = []
+        for j in range(5):
+            cordinates_row.append("(i{}, j{})".format(i, j))
+        number_square.append(cordinates_row)
+
+    return number_square
 
 
 def loops_6():
@@ -160,7 +196,13 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+    wedge = []
+    for i in range(10):
+        row = []
+        for j in range(i + 1):
+            row.append(str(j))
+        wedge.append(row)
+    return wedge
 
 
 def loops_7():
@@ -184,7 +226,17 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    return None
+    pyramid = []
+    for i in range(5):
+        rows = []
+        for j in range(9):
+            if abs(j - 4) <= i:
+                rows.append("*")
+            else:
+                rows.append(" ")
+        pyramid.append(rows)
+
+    return pyramid
 
 
 def little_printer(some_kind_of_list, exercise_name):
